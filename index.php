10d1ca77d393f05f4c766c3a754fbb9e62b1b68a
Jeremy.H <hhj962696@gmail.com>
2026-06-22 10:51:04 +0800

    step2 - 調整 CSS 高度到520px

--- index.php
+++ index.php
@@ -18,9 +18,11 @@
 	<iframe name="back" style="display:none;"></iframe>
 	<div id="all">
 		<div id="title">
-			06 月 22 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
+			 <?= date("m月 d號 l")?>| 今日瀏覽: 1 | 累積瀏覽: 36 </div>
 		<div id="title2">
-
+			<a href="index.php">
+				<img src="icon\02B01.jpg" alt="健康促進網-回首頁" title="健康促進網-回首頁">
+			</a>
 		</div>
 		<div id="mm">
 			<div class="hal" id="lef">
@@ -44,7 +46,7 @@
 		<div id="bottom">
 			本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved
 			<br>
-			服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
+			服務信箱：health@test.labor.gov.tw <img src="./icon/02B02.jpg" width="45">
 		</div>
 	</div>
 
